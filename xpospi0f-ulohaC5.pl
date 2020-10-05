fd7754707a2568b649c0f20a99930460b9811cb7
Filip Pospíšil <55882695+pospisilf@users.noreply.github.com>
2020-10-05 22:38:31 +0200

    Základy umělé inteligence úkoly

---
+++ xpospi0f-ulohaC5.pl
@@ -0,0 +1,33 @@
+ulohaC5([H|T],[H2|T2]) :-
+    intv(1,10,5).
+
+intv(X,Y,Z) :-
+    Z >= X,
+    Z =< Y,
+    X =< Y.
+
+%intv(X,Y,Z) :-
+%    Z >= X,
+%    Z =< Y,
+%    X =< Y.
+
+
+%ulohaC5([H|T],[H2|T2]) :-
+%    intv(1,10,5).
+
+%intv(X,Y,Z) :-
+%    Z >= X,
+%    Z =< Y,
+%    X =< Y.
+
+
+%ulohaC5([H|T],[H2|T2]) :-
+%    intv(1,10,5).
+%intv(X,Y,Z) :-
+%    Z >= X,
+%    Z =< Y,
+%    X =< Y.
+
+
+%ulohaC5([H|T],[H2|T2]) :-
+%    intv(1,10,5).
